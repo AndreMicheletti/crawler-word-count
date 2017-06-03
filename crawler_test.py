1d9eacc297d3dde5e014b2ec8a2ebe09453767be
AndreGameDev <andreluizmtmicheletti@gmail.com>
2017-06-03 14:49:51 -0300

    including crawler_tests to be executed with pytest

---
+++ crawler_test.py
@@ -0,0 +1,47 @@
+import pytest
+import crawler
+import requests
+
+@pytest.fixture
+def crawler_request(url, word):
+	return requests.get("http://localhost:5000?url="+url+"&word="+word)
+	
+def test_crawler_empty():
+	"""
+	GIVEN: a crawler app
+	WHEN: GET route "/" without passing arguments
+	THEN: response code should be 404 and there must be a message
+	"""
+	result = crawler_request('','')
+	assert result.status_code == 404
+	assert "message" in result.json()
+	
+def test_crawler_invalid_url():
+	"""
+	GIVEN: a crawler app
+	WHEN: GET route "/" passing into URL argument and invalid Website URL
+	THEN: response code should be 404
+	"""
+	result = crawler_request('goo.gle','')
+	assert result.status_code == 404
+	
+def test_crawler_incomplete_url():
+	"""
+	GIVEN: a crawler app
+	WHEN: GET route "/" passing URL argument without http:// in the beggining
+	THEN: the URL must be replaced to include http:// in the beggining
+	"""
+	result = crawler_request('google.com.br','')
+	assert 'http://google.com.br' in result.json()
+	
+def test_crawler_valid_url():
+	"""
+	GIVEN: a crawler app
+	WHEN: GET route "/" passing valid argument
+	THEN: response code must be 200 and there must be the result json
+	"""
+	result = crawler_request('http://google.com.br','google')
+	assert result.status_code == 200
+	assert 'http://google.com.br' in result.json() 
+	assert 'google' in result.text
+	
